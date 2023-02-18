d5517344c8507e6cfa56954d2fc13953f14654e9
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-19 00:37:06 +0900

    Button : Container, padding

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,28 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // Colors.black <- 기본 / Color(색상값) <- 커스텀
         backgroundColor: const Color(0xFF181818),
-        // Column 수직으로 배치 // Row 수평으로 배치
         body: Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: 40,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              //SizedBox 공간 주는 위젯.
               const SizedBox(
                 height: 80,
               ),
               Row(
-                //마우스 올려보면 {MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start}
-                // 파라미터 기본값으로 .start 받는 것 확인, 이걸로 뭐 써야하는지 알 수 있음.
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
-                    //Column과 Row alignment는 반대로 작용함.(flex 생각해보기)
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       const Text(
                         'Hey, Selena',
-                        //뭐가 들어가는지 모르겠으면 무조건 마우스 올려보면 답 나옴.
                         style: TextStyle(
                           color: Colors.white,
-                          //{double? fontSize} 마우스 올려보기
                           fontSize: 28,
-                          //{FontWeight? fontWeight}
                           fontWeight: FontWeight.w800,
                         ),
                       ),
@@ -50,6 +42,51 @@ class App extends StatelessWidget {
                         ),
                       ),
                     ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  //Container: div 같은 box
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 20,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
                   )
                 ],
               )
